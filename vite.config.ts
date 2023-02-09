685951cde835b8a5eb487235cf9dcadd6b04ce2f
luoxiao <luoxiao@succsoft.com>
2023-02-09 23:27:44 +0800

    fix: 修复生产环境异常

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+	plugins: [vue()],
+	build: {
+		rollupOptions: {
+			output: {
+				freeze: false, // vuepg插件会对导入的模块进行assign，所以这里不能freeze
+			}
+		}
+	}
 })
